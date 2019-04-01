9c5f8d4669d8e5a25bea3b39de482832872a2481
李思成 <34902536+lexingsen@users.noreply.github.com>
2019-04-01 11:29:38 +0800

    Create main.cpp

---
+++ lb_server/main.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: lisicheng
+    > Mail: 1050592374@qq.com 
+    > Created Time: Sat 12 Jan 2019 05:48:29 AM PST
+ ************************************************************************/
+
+#include "lb.h"
+
+int main(){
+	LBserver* server = new LBserver();
+	return 0;
+}
